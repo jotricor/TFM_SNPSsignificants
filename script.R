7ef11d92f2e196f02a4515fa68471d1cd0fc3b16
jotricor <118892133+jotricor@users.noreply.github.com>
2022-11-24 10:45:47 +0100

    Add files via upload

---
+++ script.R
@@ -0,0 +1,7 @@
+source("functions.R") 
+
+#### Use the first function of functions file to obtain the dataframe with most significant SNPs ####
+dataf1 <- ped_assocs(bed ="test_binary.bed", bim ="test_binary.bim", fam="test_binary.fam", MAF_filter = 0.01, mind_filter = 0.1, geno_filter = 0.1, hwe_filter = 0.01)
+
+#### Use the second function of functions file to obtain the most significant SNPs image in tiff format ####
+manhatan_plot(dataf1)
